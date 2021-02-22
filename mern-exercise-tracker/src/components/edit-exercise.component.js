716be07a2c81f8a4699f22506ed02d35e3bff49b
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-02-22 00:31:05 -0800

    final touches

--- mern-exercise-tracker/src/components/edit-exercise.component.js
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -1,10 +1,161 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class EditExercises extends Component {
+export default class EditExercise extends Component {
+    constructor(props) {
+        super(props); // in js classes, you need to always call super when deining the constructor of a subclass
+
+        // this is how you let "this" know what to refer to & refer to the right thing
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = { // state is how you create variables in react & when you update the state, it will automatically update page with new values
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: [] // this is here b/c on our page, there will be a dropdown menu of all the users in the database
+        }
+    }
+
+   
+    componentDidMount() { 
+        axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+            .then(response => {
+               this.setState({
+                   username: response.data.username,
+                   description: response.data.description,
+                   duration: response.data.duration,
+                   date: new Date(response.data.date) // converting into a date
+               })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+    // axios.get('http://localhost:5000/users/')
+    //     .then(response => {
+    //         if (response.data.length > 0) {
+    //             this.setState({
+    //                 users: response.data.map(user.username),
+    //             })
+    //         }
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     })
+
+    // }
+
+    // when the username is being changed, we're going to set the state (updating username)
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value // target is the textbox, value is the value of the textbox. Value will set the value of username to whatever value was in the textbox
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault(); // prevent the default html form submission behavior from taking place
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        // sending user data to the backend
+        axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
+        .then(res => console.log(res.data));
+
+        // after exercise has been submitted, we will take the person back to homepage(list of exercises)
+        window.location = '/';
+    }
+
+
     render() {
         return (
             <div>
-                <p>You are on the Edit Exercises component!</p>
+              <h3>Edit Exercise Log</h3>
+              <form onSubmit = {this.onSubmit}>
+                  <div className="form-group">
+                    <label>Username: </label>
+                    <select ref="userInput"
+                        required
+                        className="form-control"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}>
+                        {
+                            this.state.users.map(function(user) {
+                                return <option
+                                    key={user}
+                                    value={user}>{user}
+                                    </option>;
+                            }) 
+                        }
+                    ></select>
+                  </div>
+
+                  <div className="form-group">
+                    <label>Description: </label>
+                    <input type="text"
+                        required
+                        className="form-control"
+                        value={this.state.description}
+                        onChange={this.onChangeDescription}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                      <label>Duration (in minutes): </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={this.state.duration}
+                        onChange={this.onChangeDuration}
+                        />
+                  </div>
+
+
+                  <div className="form-group">
+                      <label>Date: </label>
+                      <div>
+                          <DatePicker
+                            selected={this.state.date}
+                            onChange={this.onChangeDate}
+                            />
+                      </div>
+                  </div>
+
+                  <div className="form-group">
+                      <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+                  </div>
+              </form>
             </div>
         )
     }
--- mern-exercise-tracker/src/components/exercises-list.component.js
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -2,67 +2,161 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
 export default class ExercisesList extends Component {
-    // constructor to initialize the state with an empty exercises array
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.deleteExercise = this.deleteExercise.bind(this);
+    this.deleteExercise = this.deleteExercise.bind(this)
 
-        // initialize the state
-        this.state = { exercises: [] }; // empty array of exercises
-    }
+    this.state = {exercises: []};
+  }
 
-    // get list of exercises from the database
-    // code will run before the page is rendered and add a list of exercises to the state
-    componentDidMount() {
-        axios.get('http://localhost:5000/exercises/')
-            .then(response => {
-                this.setState({ exercises: response.data }); // 
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/')
+      .then(response => {
+        this.setState({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
-    // delete exercises from the database
-    deleteExercise(id) {
-        axios.delete('http:localhost:5000/exercises/' + id)
-            .then(res => console.log(res.data));
+  deleteExercise(id) {
+    axios.delete('http://localhost:5000/exercises/'+id)
+      .then(response => { console.log(response.data)});
 
-        this.setState({ //array of exercises- filter-return certain elements back to exercises. For every element in the exercises array,we are going to return it if el._id does not = id. Whenever the id of the exercise in the exercises array does  not = the id we are deleting,we'll pass it back to the exercises array. 
-            exercises: this.state.exercises.filter(el => el._id !== id)
-        })
-    }
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
 
-    // method calls exercise list below and will return the things below
-    exerciseList() {
-        return this.state.exercises.map(currentexercise => {
-            // Exercise below is a component we created
-            return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />
-        })
-    }
+  exerciseList() {
+    return this.state.exercises.map(currentexercise => {
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>Logged Exercises</h3>
-                <table className="table">
-                    <thead className="thead-light">
-                        <tr>
-                            <th>Username</th>
-                            <th>Description</th>
-                            <th>Duration</th>
-                            <th>Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {/* tbody is going to call the exerciseList method above */}
-                        {this.exerciseList()} 
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.exerciseList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// //component #1
+// const Exercise = props => {
+//     <tr>
+//         <td>{props.exercise.username}</td>
+//         <td>{props.exercise.description}</td>
+//         <td>{props.exercise.duration}</td>
+//         <td>{props.exercise.date.substring(0,10)}</td>
+//         <td>
+//             <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props)}}>delete</a>
+//         </td>
+//     </tr>
+// }
+
+// // component #2
+// export default class ExercisesList extends Component {
+//     // constructor to initialize the state with an empty exercises array
+//     constructor(props) {
+//         super(props);
+
+//         this.deleteExercise = this.deleteExercise.bind(this);
+
+//         // initialize the state
+//         this.state = { exercises: [] }; // empty array of exercises
+//     }
+
+//     // get list of exercises from the database
+//     // code will run before the page is rendered and add a list of exercises to the state
+//     componentDidMount() {
+//         axios.get('http://localhost:5000/exercises/')
+//             .then(response => {
+//                 this.setState({ exercises: response.data }); 
+//             })
+//             .catch((error) => {
+//                 console.log(error);
+//             })
+//     }
+
+//     // delete exercises from the database
+//     deleteExercise(id) {
+//         axios.delete('http:localhost:5000/exercises/' + id)
+//             .then(res => console.log(res.data));
+
+//         this.setState({ //array of exercises- filter-return certain elements back to exercises. For every element in the exercises array,we are going to return it if el._id does not = id. Whenever the id of the exercise in the exercises array does  not = the id we are deleting,we'll pass it back to the exercises array. 
+//             exercises: this.state.exercises.filter(el => el._id !== id)
+//         })
+//     }
+
+//     // method calls exercise list below and will return the things below
+//     exerciseList() {
+//         return this.state.exercises.map(currentexercise => {
+//             // Exercise below is a component we created
+//             return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Logged Exercises</h3>
+//                 <table className="table">
+//                     <thead className="thead-light">
+//                         <tr>
+//                             <th>Username</th>
+//                             <th>Description</th>
+//                             <th>Duration</th>
+//                             <th>Date</th>
+//                             <th>Actions</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                     {/* tbody is going to call the exerciseList method above */}
+//                         {this.exerciseList()} 
+//                     </tbody>
+//                 </table>
+//             </div>
+//         )
+//     }
+// }
